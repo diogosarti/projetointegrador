27abd14e4f3f85dc274dbd1312c3719442e585f3
Diogo Sarti <diogosarti13@gmail.com>
2024-03-27 15:36:29 -0300

    fix: Servidor inicia sem necessidade de configurar arquivo env

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import fastifyStatic from '@fastify/static'
 import path from 'path'
 import 'dotenv/config'
 import { registerAllRoutes } from './routes/allRoutes'
+const host = process.env.NODE_APP_HOST || 'localhost'
+const port = process.env.NODE_APP_PORT || 3000
 
 const server = fastify()
 
@@ -15,12 +17,10 @@ server.register(fastifyStatic, {
 initViewEngine(server)
 registerAllRoutes(server)
 
-server.listen({ port: Number(process.env.NODE_APP_PORT) }, (err) => {
+server.listen({ port: Number(port) }, (err) => {
   if (err) {
     console.error(err)
     process.exit(1)
   }
-  console.log(
-    `Servidor rodando em: http://${process.env.NODE_APP_HOST}:${process.env.NODE_APP_PORT}`,
-  )
+  console.log(`Servidor rodando em: http://${host}:${port}`)
 })
